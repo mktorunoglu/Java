a9488d761f0036b0ddbb59b42ac3b484678f5f0c
Mehmet Kamil Torunoğlu <kamil.torunoglu@bil.omu.edu.tr>
2021-10-24 18:58:43 +0300

    Java

---
+++ 001_array/src/intro/Main.java
@@ -0,0 +1,13 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] cars = { "Volvo", "Mercedes", "BMW", "Renault", "Wolksvagen", "Fiat" };
+
+		for (String car : cars) {
+			System.out.println(car);
+		}
+	}
+
+}
